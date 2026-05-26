3d0d2e6555f62a2e12ea9123b138a11d8eeeede1
lewifi <lewi.hirvela@gmail.com>
2026-05-27 02:42:57 +1000

    Update trakt.js

--- netlify/functions/trakt.js
+++ netlify/functions/trakt.js
@@ -1,1 +1,27 @@
+exports.handler = async (event) => {
+  const { path = '', ...params } = event.queryStringParameters || {};
+  const qs = new URLSearchParams(params).toString();
+  const url = `https://api.trakt.tv/${path}${qs ? '?' + qs : ''}`;
 
+  try {
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        'trakt-api-version': '2',
+        'trakt-api-key': process.env.TRAKT_KEY
+      }
+    });
+    const data = await res.text();
+    return {
+      statusCode: res.status,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Cache-Control': 'public, max-age=300'
+      },
+      body: data
+    };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+};

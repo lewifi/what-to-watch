3cde865e6901662148341efe61aee7c9d5d8e02d
lewifi <lewi.hirvela@gmail.com>
2026-05-27 03:08:33 +1000

    Update trakt.js

--- netlify/functions/trakt.js
+++ netlify/functions/trakt.js
@@ -9,8 +9,8 @@ exports.handler = async (event) => {
         'Content-Type': 'application/json',
         'trakt-api-version': '2',
         'trakt-api-key': process.env.TRAKT_KEY,
-        'User-Agent': 'ephix-pulse/1.0 (https://ephix.net)'
-}
+        'User-Agent': 'Mozilla/5.0 (compatible; EphixPulse/1.0; +https://ephix.net)'
+      }
     });
     const data = await res.text();
     return {

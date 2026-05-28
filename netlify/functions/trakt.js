3a88bdb6c3910634e5a9a3886231d122e766d050
lewifi <lewi.hirvela@gmail.com>
2026-05-28 16:39:59 +1000

    Update trakt.js

--- netlify/functions/trakt.js
+++ netlify/functions/trakt.js
@@ -1,8 +1,21 @@
+const cache = {};
+const TTL = 30 * 60 * 1000;
+
 exports.handler = async (event) => {
   const { path = '', ...params } = event.queryStringParameters || {};
   const qs = new URLSearchParams(params).toString();
-  const url = `https://api.trakt.tv/${path}${qs ? '?' + qs : ''}`;
+  const cacheKey = `${path}?${qs}`;
 
+  const hit = cache[cacheKey];
+  if (hit && Date.now() - hit.t < TTL) {
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'X-Cache': 'HIT' },
+      body: hit.body
+    };
+  }
+
+  const url = `https://api.trakt.tv/${path}${qs ? '?' + qs : ''}`;
   try {
     const res = await fetch(url, {
       headers: {
@@ -13,12 +26,14 @@ exports.handler = async (event) => {
       }
     });
     const data = await res.text();
+    if (res.ok) cache[cacheKey] = { t: Date.now(), body: data };
     return {
       statusCode: res.status,
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
-        'Cache-Control': 'public, max-age=300'
+        'Cache-Control': 'public, max-age=1800',
+        'X-Cache': 'MISS'
       },
       body: data
     };
